0c63c2261826562c6376033559c504786fd4680f
AbdulWaziri <awaziri@my.centennialcollege.ca>
2022-10-02 23:47:18 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'
+  });
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'
+  });
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Us'
+  });
+});
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products'
+  });
+});
+
+/* GET Sevices page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services'
+  });
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us'
+  });
+});
+
+module.exports = router;
